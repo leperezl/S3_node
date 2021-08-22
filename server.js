45ae585094994f145415964f4fb08cb17a0146b1
Luis Perez <le.perezl@uniandes.edu.co>
2021-08-22 18:53:51 -0500

    made the htmls and figuring out the js server still

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const http= require('http');
 
 const hostname = '192.168.68.110';
 const port = 8081;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+  });
